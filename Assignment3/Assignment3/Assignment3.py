e79592dd4198a9d20baa83f702218ce3ac8b10e4
DESKTOP-3IMGBC4\Bojan Srbinoski <b2thes@hotmail.com>
2020-11-23 15:17:26 -0500

    computed class probability, computed how many times each word appears in each class, computed the conditionals for each word

--- Assignment3/Assignment3/Assignment3.py
+++ Assignment3/Assignment3/Assignment3.py
@@ -1,53 +1,87 @@
 import csv
-import re
 
 class NaiveBayes:
     training_set = []
     smoothing = 0.01
     filtered = False
-    trained_model = {}
+
+    numFactualWords = 0
+    numNonFactualWords = 0
+    numFactualTweets = 0
+    numNonFactualTweets = 0
+    vocabularySize = 0
+
+    probFactual = 0
+    probNonFactual = 0
+
+    wordClassCount = {}
+    conditionals = {}
 
     def __init__(self, training_set, filtered = False):
         self.training_set = training_set
         self.filtered = filtered
 
-    # get number of times each word appears
-    def train(self):
+        self.getWordFrequencies()
+        self.computeClassProbs()
+        self.computeConditionals()
+
+    def getWordFrequencies(self):
+    # get word frequency for each class (yes / no)
         for entry in self.training_set:
             entry_text = entry[1].split()
             for word in entry_text:
                 word = word.lower()
-                word = word.rstrip(".")
-                word = word.rstrip(",")
-                word = word.rstrip(":")
-                word = word.lstrip(".")
-                word = word.lstrip(",")
-                word = word.lstrip(":")
-                if word in self.trained_model.keys():
-                    self.trained_model[word] += 1
+                word = word.strip(".,:\"“-—'")
+                if (word, "factual") in self.wordClassCount.keys() or (word, "nonFactual") in self.wordClassCount.keys():
+                    if entry[2] == "yes":
+                        self.numFactualWords += 1
+                        self.wordClassCount[(word, "factual")] += 1
+                    else:
+                        self.numNonFactualWords += 1
+                        self.wordClassCount[(word, "nonFactual")] += 1
                 else:
-                    self.trained_model[word] = 1
+                    if entry[2] == "yes":
+                        self.numFactualWords += 1
+                        self.wordClassCount[(word, "factual")] = 1
+                        self.wordClassCount[(word, "nonFactual")] = 0
+                    else:
+                        self.numNonFactualWords += 1
+                        self.wordClassCount[(word, "factual")] = 0
+                        self.wordClassCount[(word, "nonFactual")] = 1
 
-        # filter out words that only appear once
+        # filter out words that only appear once if filtered is set to True
         words_to_remove = []
         if self.filtered:
-            for word in self.trained_model:
-                if self.trained_model[word] < 2:
+            for word in self.wordClassCount.keys():
+                if self.wordClassCount[(word[0], "factual")] + self.wordClassCount[(word[0], "nonFactual")] < 2:
                     words_to_remove.append(word)
             for word in words_to_remove:
-                del self.trained_model[word]
-
-        for x in self.trained_model:
-            print(x + " " + str(self.trained_model[x]))
+                if word[1] == "factual":
+                    self.numFactualWords -= 1
+                else:
+                    self.numNonFactualWords -= 1
+                del self.wordClassCount[word]
+        self.vocabularySize = len(self.wordClassCount) / 2
     
-    def predict(self):
-        return False
+    def computeClassProbs(self):
+    # get probability of each class (yes / no)
+        for entry in self.training_set:
+            if entry[2] == "yes":
+                self.numFactualTweets += 1
+            else:
+                self.numNonFactualTweets += 1
+        self.probFactual = self.numFactualTweets / len(self.training_set)
+        self.probNonFactual = self.numNonFactualTweets / len(self.training_set)
 
+    def computeConditionals(self):
+    # get value of conditionals for each word
+        for word in self.wordClassCount.keys():
+            if word[1] == "factual":
+                self.conditionals[word] = self.wordClassCount[word] / self.numFactualWords
+            else:
+                self.conditionals[word] = self.wordClassCount[word] / self.numNonFactualWords
 
 training_data = list(csv.reader(open('Resources/covid_training.tsv', encoding="utf-8"), delimiter='\t'))[1:]
 
-#for entry in training_data:
-#    print(entry)
-
-nb = NaiveBayes(training_data)
-nb.train()
+nb = NaiveBayes(training_data, True)
+#nb.train()
