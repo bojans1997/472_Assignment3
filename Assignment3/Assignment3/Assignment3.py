1aa510c42cb839616cb60505c1f117a231c03438
DESKTOP-3IMGBC4\Bojan Srbinoski <b2thes@hotmail.com>
2020-11-24 14:35:41 -0500

    finished training, added predict functon, added trace and evaluation files

--- Assignment3/Assignment3/Assignment3.py
+++ Assignment3/Assignment3/Assignment3.py
@@ -1,29 +1,35 @@
 import csv
+import math
 
 class NaiveBayes:
-    training_set = []
-    smoothing = 0.01
-    filtered = False
-
-    numFactualWords = 0
-    numNonFactualWords = 0
-    numFactualTweets = 0
-    numNonFactualTweets = 0
-    vocabularySize = 0
-
-    probFactual = 0
-    probNonFactual = 0
-
-    wordClassCount = {}
-    conditionals = {}
-
-    def __init__(self, training_set, filtered = False):
+    def __init__(self, training_set, test_set, filtered = False):
         self.training_set = training_set
+        self.test_set = test_set
         self.filtered = filtered
+        self.smoothing = 0.01
+        self.traceFileName = "trace_NB-BOW-OV.txt"
+        self.evalFileName = "eval_NB-BOW-OV.txt"
+
+        self.numFactualWords = 0
+        self.numNonFactualWords = 0
+        self.numFactualTweets = 0
+        self.numNonFactualTweets = 0
+        self.vocabulary = set()
+        self.vocabularySize = 0
+
+        self.probFactual = 0
+        self.probNonFactual = 0
+
+        self.wordClassCount = {}
+        self.conditionals = {}
+        if self.filtered:
+            self.traceFileName = "trace_NB-BOW-FV.txt"
+            self.evalFileName = "eval_NB-BOW-FV.txt"
 
         self.getWordFrequencies()
         self.computeClassProbs()
         self.computeConditionals()
+        self.predict()
 
     def getWordFrequencies(self):
     # get word frequency for each class (yes / no)
@@ -53,6 +59,7 @@ class NaiveBayes:
         words_to_remove = []
         if self.filtered:
             for word in self.wordClassCount.keys():
+                #print(self.wordClassCount[(word[0], "factual")] + self.wordClassCount[(word[0], "nonFactual")])
                 if self.wordClassCount[(word[0], "factual")] + self.wordClassCount[(word[0], "nonFactual")] < 2:
                     words_to_remove.append(word)
             for word in words_to_remove:
@@ -62,6 +69,8 @@ class NaiveBayes:
                     self.numNonFactualWords -= 1
                 del self.wordClassCount[word]
         self.vocabularySize = len(self.wordClassCount) / 2
+        for key in self.wordClassCount.keys():
+            self.vocabulary.add(key[0])
     
     def computeClassProbs(self):
     # get probability of each class (yes / no)
@@ -77,11 +86,80 @@ class NaiveBayes:
     # get value of conditionals for each word
         for word in self.wordClassCount.keys():
             if word[1] == "factual":
-                self.conditionals[word] = self.wordClassCount[word] / self.numFactualWords
+                self.conditionals[word] = (self.wordClassCount[word] + self.smoothing) / (self.numFactualWords + (self.vocabularySize * self.smoothing))
             else:
-                self.conditionals[word] = self.wordClassCount[word] / self.numNonFactualWords
+                self.conditionals[word] = (self.wordClassCount[word] + self.smoothing) / (self.numNonFactualWords + (self.vocabularySize * self.smoothing))
+
+    def predict(self):
+        incorrectPredictions = 0
+        predictedFactualCount = 0
+        predictedNonFactualCount = 0
+        incorrectFactualPredictions = 0
+        incorrectNonFactualPredictions = 0
+        actualFactualCount = 0
+        actualNonFactualCount = 0
+
+        f = open(self.traceFileName, "w")
+        for entry in self.test_set:
+            factualScore = math.log10(self.probFactual)
+            nonFactualScore = math.log10(self.probNonFactual)
+            for word in entry[1].split():
+                word = word.lower()
+                word = word.strip(".,:\"“-—'")
+                if word not in self.vocabulary:
+                    continue
+                factualScore = factualScore + math.log10(self.conditionals[word, "factual"])
+                nonFactualScore = nonFactualScore + math.log10(self.conditionals[word, "nonFactual"])
+            
+            likely_class = "yes"
+            score = '{:.2e}'.format(factualScore)
+            if nonFactualScore > factualScore:
+                likely_class = "no"
+                score = '{:.2e}'.format(nonFactualScore)
+            tweet_id = entry[0]
+            correct_class = entry[2]
+            label = "correct"
+            if likely_class != correct_class:
+                label = "wrong"
+                incorrectPredictions += 1
+                # for calculating precision
+                if likely_class == "yes":
+                    incorrectFactualPredictions += 1
+                else:
+                    incorrectNonFactualPredictions += 1
+            
+            # for calculating precision
+            if likely_class == "yes":
+                predictedFactualCount += 1
+            else:
+                predictedNonFactualCount += 1
+
+            # for calculating recall
+            if correct_class == "yes":
+                actualFactualCount += 1
+            else:
+                actualNonFactualCount += 1
+
+            f.write(tweet_id + "  " + likely_class + "  " + str(score) + "  " + correct_class + "  " + label + "\n")
+        f.close()
+        
+        accuracy = (len(self.test_set) - incorrectPredictions) / len(self.test_set)
+        yesPrecision = (predictedFactualCount - incorrectFactualPredictions) / predictedFactualCount
+        noPrecision = (predictedNonFactualCount - incorrectNonFactualPredictions) / predictedNonFactualCount
+        yesRecall = (actualFactualCount - incorrectNonFactualPredictions) / actualFactualCount
+        noRecall = (actualNonFactualCount - incorrectFactualPredictions) / actualNonFactualCount
+        yesF1 = (2 * yesPrecision * yesRecall) / (yesPrecision + yesRecall)
+        noF1 = (2 * noPrecision * noRecall) / (noPrecision + noRecall)
+
+        f = open(self.evalFileName, "w")
+        f.write(str(accuracy) + "\n")
+        f.write(str(yesPrecision) + "  " + str(noPrecision) + "\n")
+        f.write(str(yesRecall) + "  " + str(noRecall) + "\n")
+        f.write(str(yesF1) + "  " + str(noF1))
+        f.close()
 
 training_data = list(csv.reader(open('Resources/covid_training.tsv', encoding="utf-8"), delimiter='\t'))[1:]
+test_data = list(csv.reader(open('Resources/covid_test_public.tsv', encoding="utf-8"), delimiter='\t'))
 
-nb = NaiveBayes(training_data, True)
-#nb.train()
+filteredNB = NaiveBayes(training_data, test_data, True)
+unfilteredNB = NaiveBayes(training_data, test_data)
